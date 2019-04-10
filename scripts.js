1e083fd209ba5624d35de7e83dded557ac29d501
mohannadibrahimi <mohannad24a@gmail.com>
2019-04-10 12:08:57 -0400

    completed mobile nav

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const show = () => {
+    document.querySelector('.menu-mobile').
+    style.transform = 'translateX(0)'
+};
+
+const hide = () => {
+    document.querySelector('.menu-mobile').
+    style.transform = 'translateX(100%)'
+};
